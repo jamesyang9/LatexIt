6dd445c2834c9fb6e413f6f3b4a2cc7141adc48a
James Yang <jamesyang9@gmail.com>
2014-01-31 20:26:43 -0500

    works somewhat

--- algo/imageSegmentation.php
+++ algo/imageSegmentation.php
@@ -1,10 +1,13 @@
 <?php
-	$img = imagecreatefromjpeg("photo.JPG");
+	$img = imagecreatefrompng("photo-1.png");
 	$w = imagesx($img);
 	$h = imagesy($img);
+	$thresh = 3600;
+	//imagefilter($img, IMG_FILTER_CONTRAST, 20);
 	imagefilter($img, IMG_FILTER_GRAYSCALE);
+	$lineIs = array(h);
 
-	function pixelIntenxity($x, $y) {
+	function pixelIntensity($x, $y) {
 		global $img;
 		$rgb = imagecolorat($img, $x, $y);
 		return ($rgb & 0xFF);
@@ -13,20 +16,33 @@
 	function pixelValue($x, $y) {
 		global $w, $h, $img;
 		$l = max(0, $x - 2);
-		return pow($l, 2);
-		return 1;
+		$leftI = pixelIntensity($l, $y);
+		$thisI = pixelIntensity($x, $y);
+		return pow($leftI - $thisI, 2);
 	}
 
-	function lineValue($x, $y) {
-		global $w, $h, $img;
-		return 2;
+	function lineValue($y) {
+		global $w, $h, $img, $thresh;
+		$count = 0;
+		for ($i = 0; $i < $w; $i += 1) {
+			if (pixelValue($i, $y) > $thresh && pixelIntensity($i, $y) < 120) {
+				$count += pixelValue($i, $y);
+			}
+		}
+		return $count;
 	}
 
-	pixelIntenxity(1, 1);
-	pixelIntenxity(1, 2);
-	pixelIntenxity(1, 3);
-	pixelIntenxity(1, 4);
-
-
+	function processLines() {
+		global $lineIs, $img, $w, $h;
+		for ($i = 0; $i < $h; $i += 3) {
+			$v = lineValue($i) + lineValue($i + 1) + lineValue($i + 2);
+			lineIs[$i/3] = $v;
+		}
+	}
+	echo $w;
+	echo ". ";
+	echo $h;
+	echo "\n";
+	processLines();
 
 ?>
