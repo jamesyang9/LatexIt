0b377e75663336febb540b65496440694410d767
Will Crichton <willcrichton@cmu.edu>
2014-01-31 19:15:21 -0500

    Initial file structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTPYE html>
+<html>
+  <head>
+    <link rel="stylesheet" type="style.css">
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/app.js"></script>
+  </head>
+  <body>
+  </body>
+</html>
