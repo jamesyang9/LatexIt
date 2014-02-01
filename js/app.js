014c81de67daa5ec58a696c4c8403bb08573108e
James Yang <jamesyang9@gmail.com>
2014-02-01 05:40:45 -0500

    working hw screen

--- js/app.js
+++ js/app.js
@@ -92,6 +92,9 @@ $(function() {
         if(!isComplete()) {
             alert('Please make a choice for every line');
         };
-
+        $.post('algo/generatePDF.php', {'arr': globalData.texSeq, 'id': 54})
+        .done(function () {
+            window.open("algo/54.pdf", "popupWindow", "");
+        });
     });
 });
