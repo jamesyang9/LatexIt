c255fccb4ec39f4100e0a28cc072f8840e327dcf
James Yang <jamesyang9@gmail.com>
2014-02-01 02:18:58 -0500

    sdgsdg

--- homeworks.php
+++ homeworks.php
@@ -6,9 +6,29 @@
    </div>
    <div>
    <?php
-      for ($i = 0; $i < 6; $i++) {
-         echo $i;
+      function generateHWs($uid) {
+         global $db;
+         //$query = "SELECT * from ANSWERS WHERE id = {$id}";
+         $query = $db->prepare('SELECT * from answers where user_id :uid');
+         $query->execute(array(
+            'uid' => $uid,
+         $result = ($query->fetchAll())[0];
+         for($i = 0; $i < )
       }
+      function generateLine($piece, $id) {
+         global $db;
+         //$query = "SELECT * from ANSWERS WHERE id = {$id}";
+         $query = $db->prepare('SELECT * from answers where hw_id = :id, piece_num = :piece_num');
+         $query->execute(array(
+            'id' => $id,
+            'piece_num' => $piece_num));
+         $result = $query->fetchAll();
+
+         foreach($result as $row) {
+            echo "{$row['id']}, {$row['hw_id']}";
+         }       
+      }
+      generateLine(3, 23);
    ?>
    </div>
 <? require_once 'footer.php'; ?>
