6e8308b59d56e8227ad29f09e0e3528b03cd3f08
James Yang <jamesyang9@gmail.com>
2014-02-01 02:44:50 -0500

    sdfsd

--- homeworks.php
+++ homeworks.php
@@ -9,26 +9,30 @@
       function generateHWs($uid) {
          global $db;
          //$query = "SELECT * from ANSWERS WHERE id = {$id}";
-         $query = $db->prepare('SELECT * from answers where user_id :uid');
-         $query->execute(array(
-            'uid' => $uid,
-         $result = ($query->fetchAll())[0];
-         for($i = 0; $i < )
+         $query = $db->prepare('SELECT * from homeworks where user_id = :uid');
+         $query->execute(
+            array('uid' => $uid)
+         );
+         $result = $query->fetchAll()[0];
+         for($i = 0; $i < $result['num_pieces']; $i++) {
+            generateLine($i, $result['id']);
+         }
       }
-      function generateLine($piece, $id) {
+
+      function generateLine($piece_num, $id) {
+         echo "hi";
          global $db;
          //$query = "SELECT * from ANSWERS WHERE id = {$id}";
-         $query = $db->prepare('SELECT * from answers where hw_id = :id, piece_num = :piece_num');
-         $query->execute(array(
-            'id' => $id,
+         $query = $db->prepare('SELECT * from answers where hw_id = :id and piece_num = :piece_num');
+         $query->execute(array('id' => $id,
             'piece_num' => $piece_num));
          $result = $query->fetchAll();
 
          foreach($result as $row) {
-            echo "{$row['id']}, {$row['hw_id']}";
-         }       
+            echo "<div>{$row['id']}, <div class = 'tex'> \$x^2\$ {$row['answer']} </div> </div>";
+         }
       }
-      generateLine(3, 23);
+      generateHWs("jamesyan");
    ?>
    </div>
 <? require_once 'footer.php'; ?>
